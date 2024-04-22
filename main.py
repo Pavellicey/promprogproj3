36c8bc764fb6c56e473c5b636b11680705caeb0a
Pavellicey <151554445+Pavellicey@users.noreply.github.com>
2024-04-22 17:33:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ from flask_wtf.file import FileField, FileRequired
 from flask_wtf import FlaskForm
 from werkzeug.utils import secure_filename
 import os
+import sqlite3
 from flask import Flask
+import sqlalchemy
+from sqlalchemy.orm import Session
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+SqlAlchemyBase = sqlalchemy.orm.declarative_base()
+__factory = None
+logged_in = False
+user = dict()
 
 
 class PhotoForm(FlaskForm):
@@ -16,21 +24,40 @@ class PhotoForm(FlaskForm):
 
 
 @app.route('/')
+@app.route('/main')
 def main():
-    return render_template('main_page.html', title="Главная")
+    return render_template('main_page.html', title="Главная",
+                           big_image_style=url_for('static', filename='big_image_style.css'),
+                           logged=logged_in,
+                           img1=url_for('static', filename='data/sunbathe.jpg'),
+                           img2=url_for('static', filename='data/books.jpg'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global logged_in
+    con = sqlite3.connect('static/data/database.db')
+    cur = con.cursor()
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/success')
+        print(form.data)
+        login, password = form.data['username'], form.data['password']
+        matching = cur.execute(f"""SELECT * FROM Users WHERE login = '{login}'""").fetchall()
+        if len(matching) > 0:
+            if matching[0][0] == password:
+                logged_in = True
+                user['id'], user['login'], user['hpass'], user['role'] = matching[0]
+                return redirect('/main')
     return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/book_page')
 def book_page():
     return render_template('book_page.html')
+
+
+# @app.route('/my_books')
+# def my_books():
 
 
 if __name__ == '__main__':
